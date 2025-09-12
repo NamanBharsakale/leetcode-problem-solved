9f0facee6b0fec257de62b4383f919ac3686d0e8
Naman Bharsakale <namanbharsakale045@gmail.com>
2025-09-13 00:40:20 +0530

    Time: 0 ms (100%), Space: 10.1 MB (42.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,23 @@
-#include <algorithm>
-#include <cctype>
-#include <string>
-
-using namespace std;
 
 class Solution {
 public:
     bool isPalindrome(string s) {
-
-        transform(s.begin(),s.end(),s.begin(), [](unsigned char c){ return tolower(c); });
-        s.erase(remove_if(s.begin(),s.end(),[] (unsigned char c){return !isalnum(c); }),s.end());
-        int n = s.length();
-        for(int i = 0 ; i < n / 2; i++)
-        {
-            if(s[i] != s[n - 1 - i])
-            return false;
+        string f;
+        for(char c : s){
+            if(isalnum(c)){ 
+                f += tolower(c); 
+                }
         }
-        return true;
-
-
-        
+        int l = 0, r = f.size() - 1;
+        while(l < r)
+        {
+            if(f[l] != f[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+    return true;
+       
     }
 };
